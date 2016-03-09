9acea60501f55cb6401b41b3166343ed9384ecc0
Filip Hracek <filip.hracek@gmail.com>
2016-03-09 15:43:13 -0800

    Remove dead commented out code.

--- example/stdin_example.dart
+++ example/stdin_example.dart
@@ -14,18 +14,6 @@ main() async {
       "  \$ dart stdin_example.dart < tweets.txt\n"
       "  Here's your generated tweet.");
 
-  // List<String> lines = await stdin
-  //     .transform(new Utf8Decoder())
-  //     .transform(new LineSplitter())
-  //     .fold([], (List prev, next) {
-  //   prev.add(next);
-  //   return prev;
-  // });
-  //
-  // // Chunked conversion is currently unimplemented, otherwise we could just
-  // // use another `transform()` above.
-  // MarkovChain chain = new MarkovConverter(2).convert(lines);
-
   // Take POSIX standard input stream (or any other byte
   // stream) and use it as source text.
   var chain = await stdin
